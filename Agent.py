7b67f38f601ae2c52a61572a5390d43eb39f33b2
Arin1599 <arinpandey@gmail.com>
2025-02-16 12:07:38 +0530

    Custom Agent from scratch without any frameworks

---
+++ Agent.py
@@ -0,0 +1,20 @@
+class Agent:
+    def __init__(self,client,system):
+        self.client = client
+        self.system = system
+        self.messages = []
+        if self.system is not None:
+            self.messages.append({"role": "system", "content": self.system})
+
+    def __call__(self,message=""):
+        if message:
+            self.messages.append({"role": "user", "content": message})
+            result=self.execute()
+            self.messages.append({"role": "assistant", "content": result})
+        return result
+    def execute(self):
+        completion=self.client.chat.completions.create(
+            messages=self.messages,
+            model="llama3-70b-8192"
+        )
+        return completion.choices[0].message.content
